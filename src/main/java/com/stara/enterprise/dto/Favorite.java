96d5ceaf1c961658a5fff24ea62fc7c075b81dae
Parneet1712 <60361272+Parneet1712@users.noreply.github.com>
2020-10-03 17:12:10 -0400

    Created DTOs

---
+++ src/main/java/com/stara/enterprise/dto/Favorite.java
@@ -0,0 +1,11 @@
+package com.stara.enterprise.dto;
+
+import lombok.Data;
+
+public @Data
+class Favorite {
+    private int id;
+    private String name;
+    private String subtitle;
+    private String detail;
+}
