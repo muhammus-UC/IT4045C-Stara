1f5fcd66703e5f7599242df0195bb3cbcb405dfd
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-10-04 08:07:50 -0400

    Created Unit Tests for Favorite DAO & Service

--- src/test/java/com/stara/enterprise/FavoriteDataUnitTest.java
+++ src/test/java/com/stara/enterprise/FavoriteDataUnitTest.java
@@ -1,13 +1,26 @@
 package com.stara.enterprise;
 
+import com.stara.enterprise.dao.IFavoriteDAO;
 import com.stara.enterprise.dto.Favorite;
+import com.stara.enterprise.service.FavoriteServiceStub;
+import com.stara.enterprise.service.IFavoriteService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class FavoriteDataUnitTest {
+
+    private IFavoriteService favoriteService;
+    private Favorite favorite = new Favorite();
+
+    @MockBean
+    private IFavoriteDAO favoriteDAO;
 
     @Test
     void confirmFavorite_outputsFavorite() {
@@ -20,6 +33,56 @@ class FavoriteDataUnitTest {
         assertEquals("Transformers: Rescue Bots Academy", favorite.getName());
         assertEquals("English", favorite.getSubtitle());
         assertEquals("Running", favorite.getDetail());
+    }
+
+    @Test void fetchFavoriteByID_returnsFavoriteForID() throws Exception {
+        givenFavoriteDataAreAvailable();
+        whenFavorite318AddedIsCommunity();
+        whenSearchFavoriteWithID318();
+        thenReturnCommunityForID318();
+    }
+
+    private void givenFavoriteDataAreAvailable() throws Exception {
+        Mockito.when(favoriteDAO.save(favorite)).thenReturn(favorite);
+        favoriteService = new FavoriteServiceStub(favoriteDAO);
+    }
+
+    private void whenFavorite318AddedIsCommunity() {
+        Favorite community = new Favorite();
+        community.setId(318);
+        community.setName("Community");
+        community.setSubtitle("English");
+        community.setDetail("Ended");
+        Mockito.when(favoriteDAO.fetch(318)).thenReturn(community);
+    }
+
+    private void whenSearchFavoriteWithID318() {
+        favorite = favoriteService.fetchById(318);
+    }
+
+    private void thenReturnCommunityForID318() {
+        String name = favorite.getName();
+        assertEquals("Community", name);
+    }
+
+    @Test
+    void saveFavorite_validateReturnFavorite() throws Exception {
+        givenFavoriteDataAreAvailable();
+        whenUserCreatesANewFavoriteAndSaves();
+        thenCreateNewFavoriteRecordAndReturnIt();
+    }
+
+    private void whenUserCreatesANewFavoriteAndSaves() {
+        favorite.setId(318);
+        favorite.setName("Community");
+        favorite.setSubtitle("English");
+        favorite.setDetail("Ended");
+    }
+
+    private void thenCreateNewFavoriteRecordAndReturnIt() throws Exception {
+        Favorite createdFavorite = favoriteService.save(favorite);
+        assertEquals(favorite, createdFavorite);
+        verify(favoriteDAO, atLeastOnce()).save(favorite);
     }
 
 }
