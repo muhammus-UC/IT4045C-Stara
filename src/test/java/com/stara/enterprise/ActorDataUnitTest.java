125d3ea33501bd22472d926f2ff6015b2e513a61
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-10-06 10:43:38 -0400

    Update ActorDataUnitTest.java

--- src/test/java/com/stara/enterprise/ActorDataUnitTest.java
+++ src/test/java/com/stara/enterprise/ActorDataUnitTest.java
@@ -21,5 +21,18 @@ class ActorDataUnitTest {
         assertEquals("United States", actor.getCountry());
         assertEquals("Male", actor.getGender());
     }
+    
+    @Test
+    void confirmBradPitt_outputsBradPitt() {
+        Actor actor = new Actor();
+        actor.setId(22013);
+        actor.setName("Brad Pitt");
+        actor.setCountry("United States");
+        actor.setGender("Male");
+        assertEquals(22013, actor.getId());
+        assertEquals("Brad Pitt", actor.getName());
+        assertEquals("United States", actor.getCountry());
+        assertEquals("Male", actor.getGender());
+    }
 
 }
