bc2ed5d273c64b6d633acc59e627d5dad3f84c41
willi6m7 <willi6m7@mail.uc.edu>
2020-10-10 23:40:58 -0400

    My Code Review Suggestions

--- src/main/java/com/stara/enterprise/dao/RetrofitClientInstance.java
+++ src/main/java/com/stara/enterprise/dao/RetrofitClientInstance.java
@@ -7,12 +7,6 @@ public class RetrofitClientInstance {
     private static Retrofit retrofit;
     private static String BASE_URL = "https://api.tvmaze.com";
 
-    /*
-    * Convert the API to a Java interface.
-    * Uses Retrofit to convert the tvmaze API to a Java interface.
-    *
-    * @return the Java interface.
-     */
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
