75a9a9d9384f80ce9fd1f13894aef7401f71f1ff
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-11-01 19:39:02 -0500

    Update IFavoriteService.java

--- src/main/java/com/stara/enterprise/service/IFavoriteService.java
+++ src/main/java/com/stara/enterprise/service/IFavoriteService.java
@@ -3,6 +3,9 @@ package com.stara.enterprise.service;
 import com.stara.enterprise.dto.Favorite;
 
 import java.util.List;
+/**
+    This class creates functions for the stub to use
+**/
 
 public interface IFavoriteService {
     void delete(int id) throws Exception;
