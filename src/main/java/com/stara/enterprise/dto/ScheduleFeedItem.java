3aadea970f68742108060bd62e8fdc30feab1faf
Parneet1712 <60361272+Parneet1712@users.noreply.github.com>
2020-11-01 19:30:36 -0500

    DTO - Created DTO for Schedule

---
+++ src/main/java/com/stara/enterprise/dto/ScheduleFeedItem.java
@@ -0,0 +1,24 @@
+package com.stara.enterprise.dto;
+
+import com.google.gson.annotations.SerializedName;
+import com.stara.enterprise.dto.show.Show;
+import lombok.Data;
+
+public @Data
+class ScheduleFeedItem {
+    @SerializedName("name")
+    private String episodeName;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("airtime")
+    private String airtime;
+    @SerializedName("show")
+    private Show show;
+
+    public String toString() {
+        return "ScheduleFeedItem - Episode: " + episodeName +
+                ". Airtime: " + airtime +
+                ". URL: " + url +
+                ". Show: " + show;
+    }
+}
