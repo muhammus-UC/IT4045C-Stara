805d511c4f29cff89df25e56a92a26ab6c33f0cf
mkf145 <52715791+mkf145@users.noreply.github.com>
2020-11-26 18:06:49 -0500

    UI - Marketing - Tour - Added intro.js library and added button to activate it.

---
+++ src/main/resources/static/js/staraTour.js
@@ -0,0 +1,6 @@
+// Reference: https://github.com/usablica/intro.js
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btnTour").addEventListener("click", function() {
+        introJs().start();
+    });
+})
