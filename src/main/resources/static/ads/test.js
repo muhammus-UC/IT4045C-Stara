7f4b7826d46c03363c42781a64373d07c822249a
mkf145 <52715791+mkf145@users.noreply.github.com>
2020-11-26 14:25:20 -0500

    UI - Marketing - Moved ad JS & CSS to external files.

---
+++ src/main/resources/static/ads/test.js
@@ -0,0 +1,17 @@
+// Google Test Ad: https://developers.google.com/publisher-tag/samples/display-test-ad
+
+window.googletag = window.googletag || {cmd: []};
+
+googletag.cmd.push(function() {
+    googletag
+        .defineSlot(
+            '/6355419/Travel/Europe/France/Paris', [300, 250], 'banner-ad')
+        .addService(googletag.pubads());
+    googletag.enableServices();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    googletag.cmd.push(function() {
+        googletag.display('banner-ad');
+    });
+})
