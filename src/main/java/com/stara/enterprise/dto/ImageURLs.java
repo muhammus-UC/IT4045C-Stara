2b4171b8576cf05c46e9628baf4b8cf8e86e123e
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-11-26 19:41:10 -0500

    Suggestions from CodeReview #63. Controller - Return "error" whenever exception is caught. DTO - Added comments to ImageURLs DTO.

--- src/main/java/com/stara/enterprise/dto/ImageURLs.java
+++ src/main/java/com/stara/enterprise/dto/ImageURLs.java
@@ -4,7 +4,9 @@ import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
- * DTO to access nested image URLs.
+ * DTO to access nested image URLs in nested JSON from TVMaze API.
+ * String medium holds the URL for the medium-sized image.
+ * Other sizes are available from TVMaze API but don't need them for our usecase.
  */
 public @Data
 class ImageURLs {
