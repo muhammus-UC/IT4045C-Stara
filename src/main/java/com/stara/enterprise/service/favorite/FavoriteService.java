da69d2c7bd8ab624a5cf4b42b96402e65fc95832
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-11-30 22:28:49 -0500

    Service - Improved comments & logging & exception handling.

--- src/main/java/com/stara/enterprise/service/favorite/FavoriteService.java
+++ src/main/java/com/stara/enterprise/service/favorite/FavoriteService.java
@@ -18,15 +18,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Service that uses Firebase Service to read & write Favorite data to Firestore.
+ */
 @Service
 public class FavoriteService implements IFavoriteService {
     @Autowired
     IFirebaseService firebaseService;
 
-    private final String fireStoreCollectionUsers = "users";
-    private final String fireStoreCollectionFavorites = "favorites";
+    // Firestore collections that are used to store Favorite data.
+    // Favorite Data is stored in the following path: fireStoreCollectionUsers/emailOfUser/fireStoreCollectionFavorites/favoriteId
+    private static final String fireStoreCollectionUsers = "users";
+    private static final String fireStoreCollectionFavorites = "favorites";
 
     // Firebase Reference: https://firebase.google.com/docs/firestore/manage-data/delete-data
+
+    /**
+     * Delete Favorite in Firestore for email and id specified.
+     * Firebase Reference on deleting data: https://firebase.google.com/docs/firestore/manage-data/delete-data
+     *
+     * @param email of user who to delete Favorite for
+     * @param id of Favorite to delete
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     @Override
     @CacheEvict(value = "favorites", allEntries = true)
     public void delete(String email, String id) throws ExecutionException, InterruptedException {
@@ -36,11 +52,19 @@ public class FavoriteService implements IFavoriteService {
                 .collection(fireStoreCollectionFavorites)
                 .document(id)
                 .delete();
-
-        System.out.println("Favorite with " + id + "removed at time: " + writeResult.get().getUpdateTime());
     }
 
-    // Firebase Reference: https://firebase.google.com/docs/firestore/query-data/get-data
+    /**
+     * Get Favorites from Firestore for user by email specified.
+     * Firebase Reference on getting data: https://firebase.google.com/docs/firestore/query-data/get-data
+     *
+     * @param email of user who to retrieve Favorite(s) for
+     *
+     * @return List<Favorite> containing all favorites that belong to user
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     @Override
     @Cacheable(value = "favorites")
     public List<Favorite> fetchAll(String email) throws ExecutionException, InterruptedException {
@@ -59,12 +83,21 @@ public class FavoriteService implements IFavoriteService {
             allFavorites.add(document.toObject(Favorite.class));
         }
 
-        System.out.println("Favorites fetched");
-
         return allFavorites;
     }
 
-    // Firebase Reference: https://firebase.google.com/docs/firestore/manage-data/add-data
+
+    /**
+     * Create Favorite in Firestore for email and id specified.
+     * Firebase Reference on adding data: https://firebase.google.com/docs/firestore/manage-data/add-data
+     *
+     * @param favoriteData Map<String, String> object containing the Favorite data to write.
+     * @param email of user who to create Favorite for
+     * @param id of Favorite to create
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     @Override
     @CacheEvict(value = "favorites", allEntries = true)
     public void save(Map<String, String> favoriteData, String email, String id) throws ExecutionException, InterruptedException {
@@ -74,7 +107,5 @@ public class FavoriteService implements IFavoriteService {
                 .collection(fireStoreCollectionFavorites)
                 .document(id)
                 .set(favoriteData);
-
-        System.out.println("Favorite with " + id + "added at time: " + writeResult.get().getUpdateTime());
     }
 }
--- src/main/java/com/stara/enterprise/service/firebase/FirebaseService.java
+++ src/main/java/com/stara/enterprise/service/firebase/FirebaseService.java
@@ -8,6 +8,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 import com.google.firebase.cloud.FirestoreClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -18,19 +20,27 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- * References
+ * Service used to interact with Firebase.
+ * Firebase Auth is used for account management.
+ * Firebase Firestore is used to store Favorites data in the cloud.
+ *
+ * References:
+ * Made with help from fellow UC student Seth White's code (Github: sethwhite3)
  * https://medium.com/@prakash_chandra/crud-operations-in-springboot-using-firestore-f342e95e7802
  * https://artificialintelligence.oodles.io/dev-blogs/Expert-Guide-to-Integrating-Firebase-with-Springboot-Rest-API/
- * Also got help from fellow UC student Seth White's code (Github: sethwhite3)
  */
-
 @Service
 @Profile("!test")
 public class FirebaseService implements IFirebaseService{
-    // Path to Firebase service account private key json
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    // Path to Firebase service account private key json - THIS IS SENSITIVE DATA AND MUST NOT BE SHARED FREELY!
     private final String serviceAccountPath = "src/main/java/com/stara/enterprise/service/firebase/stara-firebase-adminsdk.json";
 
-    // @PostConstruct - Initialize this class automatically once SpringBoot has finished starting
+    /**
+     * Initializes FirebaseService
+     * PostConstruct annotation tells SpringBoot to initialize this class automatically when SpringBoot runs
+     */
     @PostConstruct
     @Override
     public void initialize() {
@@ -48,22 +58,31 @@ public class FirebaseService implements IFirebaseService{
             // Initialize Firebase for Stara
             FirebaseApp.initializeApp(options);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            log.error("Unable to read service account file for Firebase Service initialization, a FileNotFoundException occurred. Message: " + e.getMessage(), e);
+
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Unable to store service credentials for Firebase Service initialization, an IOException occurred. Message: " + e.getMessage(), e);
         }
     }
 
+    /**
+     * Get reference to Firebase Firestore that stores Favorite data.
+     * @return Firestore object referencing Firestore where Favorite data is stored.
+     */
     @Override
     public Firestore getFirestore() {
         return FirestoreClient.getFirestore();
     }
 
+    /**
+     * Used to get user information about user with uid given.
+     * @param uid - uid of user to get user information for.
+     * @return UserRecord object containing information about user, such as the user's display name or email.
+     * @throws FirebaseAuthException thrown when error in accessing Firebase Auth for any reason.
+     */
     @Override
     public UserRecord getUser(String uid) throws FirebaseAuthException {
         UserRecord userRecord = FirebaseAuth.getInstance().getUser(uid);
-
-        System.out.println("Got user with email of " + userRecord.getEmail());
 
         return userRecord;
     }
