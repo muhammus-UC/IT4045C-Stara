03267a800374d04804299c667af40bf43c081d08
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-12-01 00:29:26 -0500

    Code Optimization - Removed unused imports

--- src/main/java/com/stara/enterprise/dao/schedule/IScheduleFeedRetrofitDAO.java
+++ src/main/java/com/stara/enterprise/dao/schedule/IScheduleFeedRetrofitDAO.java
@@ -1,7 +1,6 @@
 package com.stara.enterprise.dao.schedule;
 
 import com.stara.enterprise.dto.ScheduleFeedItem;
-import com.stara.enterprise.dto.show.ShowFeedItem;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
